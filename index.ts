a96af2ce5b2f3336f811091f88e60af771a80c0a
Mary <git@mary.my.id>
2024-04-21 13:48:00 +0700

    chore: fmt

--- index.ts
+++ index.ts
@@ -1,49 +1,49 @@
 import {
-  AppBskyEmbedExternal,
-  AppBskyEmbedImages,
-  AppBskyEmbedRecord,
-  AppBskyEmbedRecordWithMedia,
-  AppBskyFeedPost,
-  AppBskyRichtextFacet,
-  BlobRef,
-  BskyAgent,
-  ComAtprotoLabelDefs,
-  ComAtprotoRepoApplyWrites,
-  ComAtprotoRepoStrongRef,
-} from "@atproto/api";
-import { TID } from "@atproto/common-web";
+	AppBskyEmbedExternal,
+	AppBskyEmbedImages,
+	AppBskyEmbedRecord,
+	AppBskyEmbedRecordWithMedia,
+	AppBskyFeedPost,
+	AppBskyRichtextFacet,
+	BlobRef,
+	BskyAgent,
+	ComAtprotoLabelDefs,
+	ComAtprotoRepoApplyWrites,
+	ComAtprotoRepoStrongRef,
+} from '@atproto/api';
+import { TID } from '@atproto/common-web';
 
-import * as dcbor from "@ipld/dag-cbor";
+import * as dcbor from '@ipld/dag-cbor';
 
-import { CID } from "multiformats/cid";
-import * as Hasher from "multiformats/hashes/hasher";
+import { CID } from 'multiformats/cid';
+import * as Hasher from 'multiformats/hashes/hasher';
 
-import { prepareObject } from "./utils";
+import { prepareObject } from './utils';
 
 // The built-in hashing functions from multiformats (`multiformats/hashes/sha2`)
 // are meant for Node.js, this is the Web Crypto API equivalent.
 const mf_sha256 = Hasher.from({
-  name: "sha2-256",
-  code: 0x12,
-  encode: async (input) => {
-    const digest = await crypto.subtle.digest("sha-256", input);
-    return new Uint8Array(digest);
-  },
+	name: 'sha2-256',
+	code: 0x12,
+	encode: async (input) => {
+		const digest = await crypto.subtle.digest('sha-256', input);
+		return new Uint8Array(digest);
+	},
 });
 
 interface Post {
-  text: string;
-  facets?: AppBskyRichtextFacet.Main[];
-  embed?:
-    | (AppBskyEmbedImages.Main & { $type: "app.bsky.embed.images" })
-    | (AppBskyEmbedExternal.Main & { $type: "app.bsky.embed.external" })
-    | (AppBskyEmbedRecord.Main & { $type: "app.bsky.embed.record" })
-    | (AppBskyEmbedRecordWithMedia.Main & {
-        $type: "app.bsky.embed.recordWithMedia";
-      });
-  langs?: string[];
-  labels?: ComAtprotoLabelDefs.SelfLabels;
-  tags?: string[];
+	text: string;
+	facets?: AppBskyRichtextFacet.Main[];
+	embed?:
+		| (AppBskyEmbedImages.Main & { $type: 'app.bsky.embed.images' })
+		| (AppBskyEmbedExternal.Main & { $type: 'app.bsky.embed.external' })
+		| (AppBskyEmbedRecord.Main & { $type: 'app.bsky.embed.record' })
+		| (AppBskyEmbedRecordWithMedia.Main & {
+				$type: 'app.bsky.embed.recordWithMedia';
+		  });
+	langs?: string[];
+	labels?: ComAtprotoLabelDefs.SelfLabels;
+	tags?: string[];
 }
 
 const did = `did:plc:ia76kvnndjutgedggx2ibrem`;
@@ -54,89 +54,89 @@ const writes: ComAtprotoRepoApplyWrites.Create[] = [];
 // Example blob, you'll get this from uploadBlob but for the sake of this demo,
 // I'll use a blob that's been uploaded to my account.
 const blob = new BlobRef(
-  CID.parse("bafkreier5qavjovxx3er6af2gzjpvam4agjgteh4md4t7g3au3quzv643y"),
-  "image/png",
-  318572
+	CID.parse('bafkreier5qavjovxx3er6af2gzjpvam4agjgteh4md4t7g3au3quzv643y'),
+	'image/png',
+	318572,
 );
 
 const posts: Post[] = [
-  {
-    text: "Post 1",
-  },
-  {
-    text: "Post 2",
-    embed: {
-      $type: "app.bsky.embed.images",
-      images: [
-        {
-          image: blob,
-          alt: "",
-        },
-      ],
-    },
-  },
+	{
+		text: 'Post 1',
+	},
+	{
+		text: 'Post 2',
+		embed: {
+			$type: 'app.bsky.embed.images',
+			images: [
+				{
+					image: blob,
+					alt: '',
+				},
+			],
+		},
+	},
 ];
 
 let reply: AppBskyFeedPost.ReplyRef | undefined;
 let tid: TID | undefined;
 
 for (let idx = 0, len = posts.length; idx < len; idx++) {
-  // The sorting behavior for multiple posts sharing the same createdAt time is
-  // undefined, so what we'll do here is increment the time by 1 for every post
-  now.setMilliseconds(idx);
+	// The sorting behavior for multiple posts sharing the same createdAt time is
+	// undefined, so what we'll do here is increment the time by 1 for every post
+	now.setMilliseconds(idx);
 
-  // Get the record key for this post
-  tid = TID.next(tid);
+	// Get the record key for this post
+	tid = TID.next(tid);
 
-  const rkey = tid.toString();
-  const post = posts[idx];
+	const rkey = tid.toString();
+	const post = posts[idx];
 
-  const record: AppBskyFeedPost.Record = {
-    // IMPORTANT: $type has to exist, CID is calculated with the `$type` field
-    // present and will produce the wrong CID if you omit it.
-    // `createRecord` and `applyWrites` currently lets you omit this and it'll add
-    // it for you, but we want to avoid that here.
-    $type: "app.bsky.feed.post",
-    createdAt: now.toISOString(),
-    ...post,
-    reply: reply,
-  };
+	const record: AppBskyFeedPost.Record = {
+		// IMPORTANT: $type has to exist, CID is calculated with the `$type` field
+		// present and will produce the wrong CID if you omit it.
+		// `createRecord` and `applyWrites` currently lets you omit this and it'll add
+		// it for you, but we want to avoid that here.
+		$type: 'app.bsky.feed.post',
+		createdAt: now.toISOString(),
+		...post,
+		reply: reply,
+	};
 
-  writes.push({
-    collection: "app.bsky.feed.post",
-    rkey: rkey,
-    value: record,
-  });
+	writes.push({
+		collection: 'app.bsky.feed.post',
+		rkey: rkey,
+		value: record,
+	});
 
-  // Retrieve the next reply ref
-  if (idx !== len - 1) {
-    // IMPORTANT: `prepareObject` prepares the record to be hashed by removing
-    // fields with undefined value, and converting BlobRef instances to the
-    // right IPLD representation.
-    const prepared = prepareObject(record);
+	// Retrieve the next reply ref
+	if (idx !== len - 1) {
+		// IMPORTANT: `prepareObject` prepares the record to be hashed by removing
+		// fields with undefined value, and converting BlobRef instances to the
+		// right IPLD representation.
+		const prepared = prepareObject(record);
 
-    // 1. Encode the record into DAG-CBOR format
-    const encoded = dcbor.encode(prepared);
+		// 1. Encode the record into DAG-CBOR format
+		const encoded = dcbor.encode(prepared);
 
-    // 2. Hash the record in SHA-256 (code 0x12)
-    const digest = await mf_sha256.digest(encoded);
+		// 2. Hash the record in SHA-256 (code 0x12)
+		const digest = await mf_sha256.digest(encoded);
 
-    // 3. Create a CIDv1, specifying DAG-CBOR as content (code 0x71)
-    const cid = CID.createV1(0x71, digest);
+		// 3. Create a CIDv1, specifying DAG-CBOR as content (code 0x71)
+		const cid = CID.createV1(0x71, digest);
 
-    // 4. Get the Base32 representation of the CID (`b` prefix)
-    const b32 = cid.toString();
+		// 4. Get the Base32 representation of the CID (`b` prefix)
+		const b32 = cid.toString();
 
-    const ref: ComAtprotoRepoStrongRef.Main = {
-      cid: b32,
-      uri: `at://${did}/app.bsky.feed.post/${rkey}`,
-    };
+		const ref: ComAtprotoRepoStrongRef.Main = {
+			cid: b32,
+			uri: `at://${did}/app.bsky.feed.post/${rkey}`,
+		};
 
-    reply = {
-      root: reply ? reply.root : ref,
-      parent: ref,
-    };
-  }
+		reply = {
+			root: reply ? reply.root : ref,
+			parent: ref,
+		};
+	}
 }
 
 console.dir(writes, { depth: Infinity });
--- utils.ts
+++ utils.ts
@@ -1,63 +1,63 @@
-import { BlobRef } from "@atproto/api";
+import { BlobRef } from '@atproto/api';
 
 export const isPlainObject = (v: any): boolean => {
-  if (typeof v !== "object" || v === null) {
-    return false;
-  }
+	if (typeof v !== 'object' || v === null) {
+		return false;
+	}
 
-  const proto = Object.getPrototypeOf(v);
-  return proto === Object.prototype || proto === null;
+	const proto = Object.getPrototypeOf(v);
+	return proto === Object.prototype || proto === null;
 };
 
 export const prepareObject = (v: any): any => {
-  // IMPORTANT: BlobRef#ipld() returns the correct object we need for hashing,
-  // the API client will convert this for you but we're hashing in the client,
-  // so we need it *now*.
-  if (v instanceof BlobRef) {
-    return v.ipld();
-  }
+	// IMPORTANT: BlobRef#ipld() returns the correct object we need for hashing,
+	// the API client will convert this for you but we're hashing in the client,
+	// so we need it *now*.
+	if (v instanceof BlobRef) {
+		return v.ipld();
+	}
 
-  // Walk through arrays
-  if (Array.isArray(v)) {
-    let pure = true;
+	// Walk through arrays
+	if (Array.isArray(v)) {
+		let pure = true;
 
-    const mapped = v.map((value) => {
-      if (value !== (value = prepareObject(value))) {
-        pure = false;
-      }
+		const mapped = v.map((value) => {
+			if (value !== (value = prepareObject(value))) {
+				pure = false;
+			}
 
-      return value;
-    });
+			return value;
+		});
 
-    return pure ? v : mapped;
-  }
+		return pure ? v : mapped;
+	}
 
-  // Walk through plain objects
-  if (isPlainObject(v)) {
-    const obj: any = {};
+	// Walk through plain objects
+	if (isPlainObject(v)) {
+		const obj: any = {};
 
-    let pure = true;
+		let pure = true;
 
-    for (const key in v) {
-      let value = v[key];
+		for (const key in v) {
+			let value = v[key];
 
-      // `value` is undefined
-      if (value === undefined) {
-        pure = false;
-        continue;
-      }
+			// `value` is undefined
+			if (value === undefined) {
+				pure = false;
+				continue;
+			}
 
-      // `prepareObject` returned a value that's different from what we had before
-      if (value !== (value = prepareObject(value))) {
-        pure = false;
-      }
+			// `prepareObject` returned a value that's different from what we had before
+			if (value !== (value = prepareObject(value))) {
+				pure = false;
+			}
 
-      obj[key] = value;
-    }
+			obj[key] = value;
+		}
 
-    // Return as is if we haven't needed to tamper with anything
-    return pure ? v : obj;
-  }
+		// Return as is if we haven't needed to tamper with anything
+		return pure ? v : obj;
+	}
 
-  return v;
+	return v;
 };
